f6e02c91a07fabc481a6105296e7a791d7b82fdf
Grey Baker <greysteil@gmail.com>
2014-11-24 23:06:34 +0000

    Bump GoCardless version

--- config.dist.php
+++ config.dist.php
@@ -2,7 +2,7 @@
 /* Only really necessary if you want to run the tests */
 return [
     "baseUrl" => "https://api-sandbox.gocardless.com/",
-    "gocardlessVersion" => "2014-10-03",
+    "gocardlessVersion" => "2014-11-03",
     "username" => "",
     "password" => ""
 ];
