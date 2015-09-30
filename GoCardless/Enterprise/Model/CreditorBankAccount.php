597d2397cf74c25e502dba6f80622d75489641b8
84pixels <84pixels@gmail.com>
2015-09-30 10:38:27 +0200

    Update CreditorBankAccount.php
    Add $bank_code & $iban.

--- GoCardless/Enterprise/Model/CreditorBankAccount.php
+++ GoCardless/Enterprise/Model/CreditorBankAccount.php
@@ -45,6 +45,16 @@ class CreditorBankAccount extends Model
      * @var string
      */
     protected $bank_name;
+    
+    /**
+     * @var string
+     */    
+    protected $bank_code;
+    
+    /**
+     * @var string
+     */    
+    protected $iban; 
 
     /**
      * @param string $account_holder_name
@@ -157,4 +167,36 @@ class CreditorBankAccount extends Model
     {
         return $this->sort_code;
     }
+    
+    /**
+     * @param string $bank_code
+     */    
+        public function setBankCode($bank_code)
+    {
+        $this->bank_code = $bank_code;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getBankCode()
+    {
+        return $this->bank_code;
+    } 
+    
+    /**
+     * @param string $iban
+     */  
+    public function setIban($iban)
+    {
+        $this->iban = $iban;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getIban()
+    {
+        return $this->iban;
+    } 
 } 
